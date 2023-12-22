e4dc1f8e684415ae419c4a76ee92f084cac1b494
Andrew Loable <andrew.loable@gmail.com>
2023-12-22 14:44:23 +0800

    658 weather data

--- 3D_lotto_weather.py
+++ 3D_lotto_weather.py
@@ -3,13 +3,13 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
 # Manually set the column names
-columns = ['temp', 'pressure', 'humidity', 'slot', 'target']
+columns = ['temp', 'pressure', 'humidity', 'slot', 'partofday', 'target']
 
 # Load your dataset (replace 'your_dataset.csv' with your actual file)
-data = pd.read_csv('658_weather.csv', names=columns, header=0)
+data = pd.read_csv('3D-weather.csv', names=columns, header=0)
 
 # Assuming 'target' is the column you want to predict
-X = data[['temp', 'pressure', 'humidity', 'slot']]
+X = data[['temp', 'pressure', 'humidity', 'slot', 'partofday']]
 y = data['target']
 
 # Create a Decision Tree classifier
@@ -22,10 +22,11 @@ classifier.fit(X.values, y)
 temperature = float(input("Enter temperature: "))
 pressure = float(input("Enter pressure: "))
 humidity = float(input("Enter humidity: "))
+partofday = float(input("Enter part of day 1=morning, 2=afternoon, 3=evening: "))
 slot = float(input("Enter slot:"))
 
 # Make a prediction
-prediction = classifier.predict([[temperature, pressure, humidity, slot]])
+prediction = classifier.predict([[temperature, pressure, humidity, slot, partofday]])
 
 # Display the predicted classification
 print(f'The predicted classification is: {prediction[0]}')
